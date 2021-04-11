cd9cc900975fb27bac43afdf48366a5d1f8ff790
Rubén González <artorias201001@gmail.com>
2021-04-11 13:14:32 -0500

    Added validation function to script.js for signUp

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let inputEmail = document.getElementById("inputEmail");
 let inputPassword = document.getElementById("inputPassword");
+let inputPassword1 = document.getElementById("inputPassword1");
 
 const validateBoxes = () => {
     if(inputEmail.value === "") {
@@ -10,6 +11,15 @@ const validateBoxes = () => {
         alert("El campo de la contraseña no puede estar vacío");
         return false;
     }
+    return true;
+}
+
+const isPassEqual = () => {
+    if(inputPassword != inputPassword1){
+        alert("Las contraseñas deben ser iguales");
+        return false;
+    }
+
     return true;
 }
 
